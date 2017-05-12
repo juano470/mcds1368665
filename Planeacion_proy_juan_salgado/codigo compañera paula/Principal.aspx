115a4164be7818d0603a52a89f6a5853735a7936
juano470 <juano470@gmail.com>
2017-05-11 20:50:26 -0500

    Asi me lo entrego la compañera Paula

---
+++ Planeacion_proy_juan_salgado/codigo compañera paula/Principal.aspx
@@ -0,0 +1,100 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Principal.aspx.cs" Inherits="Vista_Principal" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+	<style type="text/css">
+		.auto-style1 {
+			width: 100%;
+		}
+		.auto-style2 {
+			width: 299px;
+		}
+	</style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		<table class="auto-style1">
+			<tr>
+				<td class="auto-style2">
+					<asp:Label ID="Label1" runat="server" Font-Bold="True" Font-Size="XX-Large" ForeColor="Blue" Text="Cajero Automático"></asp:Label>
+				</td>
+				<td>&nbsp;</td>
+			</tr>
+			<tr>
+				<td>
+					<asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Consultar Saldo" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					<asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Retiros" />
+				</td>
+				<td>
+					<asp:HiddenField ID="HDSaldo" runat="server" />
+				</td>
+			</tr>
+		</table>
+		<asp:MultiView ID="MultiView1" runat="server">
+			<asp:View ID="View1" runat="server">
+				Ingrese Su Contraseña<br />
+				<asp:TextBox ID="TxtPass" runat="server"></asp:TextBox>
+				&nbsp;&nbsp;&nbsp;&nbsp;
+				<asp:Button ID="btnEntrar" runat="server" OnClick="Button3_Click" Text="Ingresar" />
+				&nbsp;
+				<asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+			</asp:View>
+			<asp:View ID="View2" runat="server">
+				Su Saldo Es:<br />
+				<asp:TextBox ID="TxtSaldo" runat="server"></asp:TextBox>
+				&nbsp;
+			</asp:View>
+			<asp:View ID="View3" runat="server">
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				<table style="width:100%;">
+					<tr>
+						<td>
+							<asp:Button ID="Button4" runat="server" OnClick="Button4_Click" Text="10.000" />
+						</td>
+						<td>
+							<asp:Button ID="Button7" runat="server" OnClick="Button7_Click" Text="100.000" />
+						</td>
+						<td>
+							<asp:Button ID="Button10" runat="server" OnClick="Button10_Click" Text="600.000" />
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<asp:Button ID="Button5" runat="server" OnClick="Button5_Click" style="height: 26px" Text="20.000" />
+						</td>
+						<td>
+							<asp:Button ID="Button8" runat="server" OnClick="Button8_Click" Text="200.000" />
+						</td>
+						<td>Otro Valor:
+							<pre style="padding: 5px; margin: 0px; font-style: normal; font-weight: normal; overflow: auto; word-wrap: normal; font-family: Consolas, Courier, monospace !important; color: rgb(0, 0, 0); font-size: 13px; font-variant-ligatures: normal; font-variant-caps: normal; letter-spacing: normal; orphans: 2; text-align: left; text-indent: 0px; text-transform: none; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; text-decoration-style: initial; text-decoration-color: initial;"><asp:TextBox ID="TextBox3" runat="server" OnTextChanged="TextBox3_TextChanged"></asp:TextBox> <asp:Button ID="Button11" runat="server" OnClick="Button11_Click" Text="Retirar" /></pre>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<asp:Button ID="Button6" runat="server" OnClick="Button6_Click" Text="50.000" />
+						</td>
+						<td>
+							<asp:Button ID="Button9" runat="server" OnClick="Button9_Click" Text="400.000" />
+						</td>
+						<td>&nbsp;Saldo Actual:<br />
+							<asp:TextBox ID="TxtPassRetiro" runat="server"></asp:TextBox>
+							&nbsp;<asp:Button ID="btnPasswd" runat="server" OnClick="btnPasswd_Click" Text="Limpiar" />
+						</td>
+					</tr>
+				</table>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+			</asp:View>
+		</asp:MultiView>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Planeacion_proy_juan_salgado/codigo compañera paula/Principal.aspx.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+
+
+public partial class Vista_Principal : System.Web.UI.Page
+{
+	protected static StringBuilder sbMensaje = new StringBuilder();
+	string Password = "123456";
+	float Saldo;
+	 
+	protected void Page_Load(object sender, EventArgs e)
+	{
+		//Refresh
+		if (!IsPostBack)
+		{
+			
+			HDSaldo.Value = "1000000";
+		}
+	}
+
+	protected void Button1_Click(object sender, EventArgs e)
+	{
+		MultiView1.ActiveViewIndex = 0;
+	}
+
+	protected void Button3_Click(object sender, EventArgs e)
+	{
+		//Consulta Saldo Y Cobra 3000 Por Consulta
+		if (TxtPass.Text == Password)
+		{
+			
+			TxtPass.Text = generarClaveSHA1(TextBox4.Text);
+			HDSaldo.Value = (float.Parse(HDSaldo.Value) - 3000).ToString();
+			TxtSaldo.Text = HDSaldo.Value.ToString();
+			MultiView1.ActiveViewIndex = 1;
+		}
+		
+	}
+	//Limpia Datos 
+	protected void btnPasswd_Click(object sender, EventArgs e)
+	{
+		if (TxtPassRetiro.Text == Password)
+		{
+			LimpiarDatos();
+		}
+	}
+
+	protected void Button4_Click(object sender, EventArgs e)
+	{
+		//Retiro Cuenta
+		HDSaldo.Value = (float.Parse(HDSaldo.Value) - 10000).ToString();
+		ImprimirMensaje("Retiro Exitoso");
+		TxtPassRetiro.Text = HDSaldo.Value.ToString();
+	}
+
+	protected void Button5_Click(object sender, EventArgs e)
+	{
+		//Retiro Cuenta
+		if (float.Parse(HDSaldo.Value) > 45000)
+		{
+			HDSaldo.Value = (float.Parse(HDSaldo.Value) - 20000).ToString();
+			ImprimirMensaje("Retiro Exitoso");
+			TxtPassRetiro.Text = HDSaldo.Value.ToString();
+		}
+		else {
+			ImprimirMensaje("Retiro No Exitoso");
+		}
+	}
+
+	protected void Button6_Click(object sender, EventArgs e)
+	{
+		//Retiro Cuenta
+		if (float.Parse(HDSaldo.Value) > 525000)
+		{
+			HDSaldo.Value = (float.Parse(HDSaldo.Value) - 50000).ToString();
+			ImprimirMensaje("Retiro Exitoso");
+			TxtPassRetiro.Text = HDSaldo.Value.ToString();
+		}
+		else
+		{
+			ImprimirMensaje("Retiro No Exitoso");
+		}
+	}
+
+		protected void Button2_Click(object sender, EventArgs e)
+		{
+			MultiView1.ActiveViewIndex = 2;
+		}
+
+		protected void Button11_Click(object sender, EventArgs e)
+		{
+		//No Permite Exceder El Monto Mas De 600000
+			if (float.Parse(TextBox3.Text)>600000)
+			{
+				ImprimirMensaje("Ha Excedido El Monto Permitido");
+				LimpiarDatos();
+			}
+		}
+
+
+		protected void TextBox3_TextChanged(object sender, EventArgs e)
+		{
+		}
+	//Imprimir Mensajes En Pantalla Emergente
+	protected void ImprimirMensaje(string mensaje)
+	{
+		sbMensaje.Clear();
+		sbMensaje.Append("<script type='text/javascript'>");
+		sbMensaje.AppendFormat("alert('{0}');", mensaje);
+		sbMensaje.Append("</script>");
+		ClientScript.RegisterClientScriptBlock(this.GetType(), "mensaje", sbMensaje.ToString());
+	}
+	protected void LimpiarDatos()
+	{
+		TextBox3.Text = "";
+		btnPasswd.Text = "";
+
+	}
+	
+	//Método para generar una clave con un hash SHA-1, a partir de una cadena dada
+    private string generarClaveSHA1(string cadena)
+	{
+
+		UTF8Encoding enc = new UTF8Encoding();
+		byte[] data = enc.GetBytes(cadena);
+		byte[] result;
+
+		SHA1CryptoServiceProvider sha = new SHA1CryptoServiceProvider();
+
+	    result = sha.ComputeHash(data);
+
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < result.Length; i++)
+		{
+
+			 //Convertimos los valores en hexadecimal
+			 // cuando tiene una cifra hay que rellenarlo con cero
+			 //para que siempre ocupen dos dígitos.
+			if (result[i] < 16)
+			{
+				sb.Append("0");
+			}
+			sb.Append(result[i].ToString("x"));
+		}
+
+		//Devolvemos la cadena con el hash en mayúsculas.
+		return sb.ToString().ToUpper();
+	}
+
+
+	protected void Button7_Click(object sender, EventArgs e)
+	{
+		//Retiro De Cuenta
+		if (float.Parse(HDSaldo.Value) > 125000)
+		{
+			HDSaldo.Value = (float.Parse(HDSaldo.Value) - 100000).ToString();
+			ImprimirMensaje("Retiro Exitoso");
+			TxtPassRetiro.Text = HDSaldo.Value.ToString();
+		}
+		else
+		{
+			ImprimirMensaje("Retiro No Exitoso");
+		}
+
+	}
+
+	protected void Button8_Click(object sender, EventArgs e)
+	{
+		//Retiro De Cuenta
+		if (float.Parse(HDSaldo.Value) > 225000)
+		{
+			HDSaldo.Value = (float.Parse(HDSaldo.Value) - 200000).ToString();
+			ImprimirMensaje("Retiro Exitoso");
+			TxtPassRetiro.Text = HDSaldo.Value.ToString();
+		}
+		else
+		{
+			ImprimirMensaje("Retiro No Exitoso");
+		}
+	}
+
+	protected void Button9_Click(object sender, EventArgs e)
+	{
+		//Retiro De Cuenta
+		if (float.Parse(HDSaldo.Value) > 425000)
+		{
+			HDSaldo.Value = (float.Parse(HDSaldo.Value) - 400000).ToString();
+			ImprimirMensaje("Retiro Exitoso");
+			TxtPassRetiro.Text = HDSaldo.Value.ToString();
+		}
+		else
+		{
+
+		}
+	}
+
+	protected void Button10_Click(object sender, EventArgs e)
+	{
+		//Retiro De Cuenta
+		if (float.Parse(HDSaldo.Value) > 425000)
+		{
+			HDSaldo.Value = (float.Parse(HDSaldo.Value) - 600000).ToString();
+			ImprimirMensaje("Retiro Exitoso");
+			TxtPassRetiro.Text = HDSaldo.Value.ToString();
+		}
+		else
+		{
+			ImprimirMensaje("Retiro No Exitoso");
+		}
+	}
+}
